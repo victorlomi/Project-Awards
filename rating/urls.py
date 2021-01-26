e3410fa72eaa5f1bfbb3df26f94b90688bbf0365
victorlomi <victorlominyo@gmail.com>
2021-01-26 22:22:35 +0300

    create profile model with most of the properties

--- rating/urls.py
+++ rating/urls.py
@@ -1,7 +1,13 @@
-from django.urls import path
-
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-    path('', views.homepage, name='homepage')
+    path('', views.homepage, name='homepage'),
+    path('signup/', views.signup, name="signup"),
+    path('', include('django.contrib.auth.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- rating/views.py
+++ rating/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def homepage(request):
     return render(request, "homepage.html")
+
+def signup(request):
+    return render(request, "registration/signup.html")
