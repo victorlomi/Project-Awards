a222f1035c607d34f3ada40192f22e97eb3c6db2
victorlomi <victorlominyo@gmail.com>
2021-01-26 20:28:49 +0300

    add bootstrap4 to the project

--- awards/settings.py
+++ awards/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'rating.apps.RatingConfig',
+    'bootstrap4'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
