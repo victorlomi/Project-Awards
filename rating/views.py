9a83e001549e821d94e8faaab7fbdacb948065d9
victorlomi <victorlominyo@gmail.com>
2021-01-26 21:54:20 +0300

    add basic navbar with sign up and log in

--- rating/views.py
+++ rating/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def homepage(request):
-    return HttpResponse("welcome to the best website")
+    return render(request, "base.html")
