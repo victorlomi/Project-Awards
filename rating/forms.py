1f40119993fc199f7f486d2be12e6895bd1d3aa7
victorlomi <victorlominyo@gmail.com>
2021-01-27 05:36:57 +0300

    complete building the extended signup form

--- rating/forms.py
+++ rating/forms.py
@@ -6,7 +6,9 @@ from .models import Profile
 class SignUpForm(UserCreationForm):
     email = forms.EmailField(max_length=200, help_text="Enter a valid email address.")
     profile_photo = forms.ImageField(label="Select a profile image")
+    bio = forms.CharField(max_length=500, label="Bio", help_text="Tell us about yourself.", widget=forms.Textarea(
+            attrs={"placeholder": "Add a description of yourself",}))
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2', 'profile_photo')
+        fields = ('username', 'email', 'password1', 'password2', 'profile_photo', 'bio')
